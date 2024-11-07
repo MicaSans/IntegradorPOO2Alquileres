d115534a2f642cbf37825a4c20a4bfb7d35d1dcb
Sebastian Correa <sebastiancorreaemanuel@gmail.com>
2024-11-06 21:19:54 -0300

    mismo

--- test/ar/edu/unq/integrador/alquileres/SistemaDeAlquilerTest.java
+++ test/ar/edu/unq/integrador/alquileres/SistemaDeAlquilerTest.java
@@ -23,7 +23,8 @@ class SistemaDeAlquilerTest {
 		// testGenerarPublicacion() "No me anda el mockito y por eso creo las clases"
 		servicios = Arrays.asList("Gas","Agua");
 		inmueble = new Inmueble(200, "Argentina", "Quilmes", "Conesa 722" , 5, servicios, "Casa", usuario);
-		publicacion = new Publicacion(inmueble, )
+		fotos = new Foto()
+		publicacion = new Publicacion(inmueble, fotos)
 		
 	}
 
