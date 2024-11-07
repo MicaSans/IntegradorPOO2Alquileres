3a1344a397d998b565db829e5afeee3b863eaf16
Sebastian Correa <sebastiancorreaemanuel@gmail.com>
2024-11-06 22:08:20 -0300

    Busqueda

--- src/ar/edu/unq/integrador/alquileres/Busqueda.java
+++ src/ar/edu/unq/integrador/alquileres/Busqueda.java
@@ -3,7 +3,7 @@ package ar.edu.unq.integrador.alquileres;
 import java.util.List;
 
 public class Busqueda {
-
+	
 	public List<Publicacion> filtrarPublicaciones(List<Publicacion> publicaciones) {
 		
 		return null;
