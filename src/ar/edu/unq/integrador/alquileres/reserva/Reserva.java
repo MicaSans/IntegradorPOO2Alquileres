4c35fca4539dd305a131aba5f25173238229c933
MicaSans <mica.sans@hotmail.com>
2024-12-09 03:10:51 -0300

    Mejoras de encapsulamiento

--- src/ar/edu/unq/integrador/alquileres/reserva/Reserva.java
+++ src/ar/edu/unq/integrador/alquileres/reserva/Reserva.java
@@ -17,6 +17,7 @@ public class Reserva {
 
 	public Reserva() {
 		//Creo este constructor con la finalidad de poder usar un SPY en el test
+	
 	}
 	
 	public Reserva(Usuario inquilino, Publicacion publicacion, RangoDeFechas fechasDeAlquiler) {
@@ -24,22 +25,27 @@ public class Reserva {
 		this.publicacion = publicacion;
 		this.fechasDeAlquiler = fechasDeAlquiler; 
 		this.estado = new Pendiente();
+	
 	}
 
 	public String verCiudadDeReserva() {
 		return this.getPublicacion().getInmueble().getCiudad();
+	
 	}
 
 	public Publicacion getPublicacion() {
 		return this.publicacion;
+	
 	}	
 	
 	public Estado getEstado() {
 		return this.estado;
+	
 	}
 	
 	public void aceptarReserva() {
 		this.getEstado().aceptarReserva(this);	
+	
 	}
 
 	public void rechazarReserva() {
@@ -50,34 +56,47 @@ public class Reserva {
 	public String cancelarReserva() {
 		this.getEstado().cancelarReserva(this);
 		return this.getPublicacion().cancelarReserva(this.getFechas());
+	
 	}
 
 	public void realizadoDeCheckOut() {
 		this.getEstado().checkOut(this);
+	
 	}
 	
 	public Boolean fueAlquilada() {
 		return this.getEstado().fueAlquilada(this);
+	
 	}
 
 	public Usuario getInquilino() {
 		return this.inquilino;
+	
 	}
 
 	public LocalDate getFechaIngreso() {
 		return this.getFechas().getInicio();
+	
 	}
 
 	public RangoDeFechas getFechas() {
 		return this.fechasDeAlquiler;
+	
 	}
 
 	public void setEstado(Estado estado) {
 		this.estado = estado;
+	
 	}
 
 	public void setNuevoInquilino(Usuario inquilino) {
 		this.inquilino = inquilino;
+	
+	}
+
+	public String getTipoInmueble() {
+		return this.getPublicacion().getTipoInmueble();
+	
 	}
 
 }
