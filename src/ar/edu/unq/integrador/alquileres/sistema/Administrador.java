b9cc3b240c0ce5496790bdb0bbd66ce542c0339e
MicaSans <mica.sans@hotmail.com>
2024-12-12 12:20:42 -0300

    Modifico Administrador métodos que rompen encapsulamiento

--- src/ar/edu/unq/integrador/alquileres/sistema/Administrador.java
+++ src/ar/edu/unq/integrador/alquileres/sistema/Administrador.java
@@ -10,43 +10,54 @@ public class Administrador {
 
 	public Administrador(SistemaDeAlquiler sistema) {
 		this.sistema = sistema;
+	
 	}
 
 	private SistemaDeAlquiler getSistema() {
 		return this.sistema;
+	
 	}
 	
 	public void darDeAltaServicio(String servicio) {
 		this.getSistema().setServicio(servicio);
+	
 	}
 
 	public void darDeAltaCategoriaPuntuacion(String categoria) {
 		this.getSistema().setCategoriaPuntuacion(categoria);
+	
 	}
 
 	public void darDeAltaTipoInmueble(String tipoInmueble) {
 		this.getSistema().setTipoInmueble(tipoInmueble);
+	
 	}
 
 	public List<Publicacion> obtenerInmueblesLibres() {
-		List<Publicacion> publicaciones = this.getSistema().getPublicaciones().stream()
+		List<Publicacion> publicaciones = getPublicacionesDelSistema().stream()
 			.filter(publi -> publi.getDiasOcupados().size() == 0)
 			.toList();
 		return publicaciones;
 		
 	}
 
+	private List<Publicacion> getPublicacionesDelSistema() {
+		return this.getSistema().getPublicaciones();
+	
+	}
+
 	public Double obtenerTasaOcupacion() {
-		Double tasa = Double.valueOf(obtenerInmueblesAlquilados().size()) / this.getSistema().getPublicaciones().size();
+		Double tasa = Double.valueOf(obtenerInmueblesAlquilados().size()) / getPublicacionesDelSistema().size();
 		return  tasa;
+	
 	}
 
 	private List<Publicacion> obtenerInmueblesAlquilados() {
-		List<Publicacion> publicaciones = this.getSistema().getPublicaciones().stream()
+		List<Publicacion> publicaciones = getPublicacionesDelSistema().stream()
 				.filter(publi -> publi.getDiasOcupados().size() != 0)
 				.toList();
 			return publicaciones;
+	
 	}
-
 	
 }
